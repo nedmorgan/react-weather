93896caf964c4a95486d1544f648b7356faad659
nedmorgan <nmorgan1987@gmail.com>
2019-09-04 20:12:16 -0400

    updated api info

--- client/src/components/WeatherSearch.jsx
+++ client/src/components/WeatherSearch.jsx
@@ -4,13 +4,11 @@ import axios from 'axios'
 import CurrentWeather from './CurrentWeather'
 const WEATHER_KEY = process.env.REACT_APP_KEY
 
-
 export default class WeatherSearch extends Component {
-
   state = {
     currentWeather: {},
     currentZip: '',
-    displayWeatherData: false,
+    displayWeatherData: false
   }
 
   componentDidMount() {
@@ -18,37 +16,45 @@ export default class WeatherSearch extends Component {
   }
 
   getWeatherData = () => {
-    axios.get(`http://api.openweathermap.org/data/2.5/weather?zip=${this.state.currentZip.currentZip},us&units=imperial&appid=${WEATHER_KEY}`).then(res => {
-      this.setState({ currentWeather: res.data })
-    }).then(res => {
-      this.setState({ displayWeatherData: !this.state.displayWeatherData })
-    })
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?zip=${
+          this.state.currentZip.currentZip
+        },us&units=imperial&appid=${WEATHER_KEY}`
+      )
+      .then(res => {
+        this.setState({ currentWeather: res.data })
+      })
+      .then(res => {
+        this.setState({ displayWeatherData: !this.state.displayWeatherData })
+      })
   }
 
-  handleChange = (e) => {
+  handleChange = e => {
     const zipcode = { ...this.state.currentZip }
     zipcode[e.target.name] = e.target.value
     this.setState({ currentZip: zipcode })
   }
 
-  handleNewWeather = (e) => {
+  handleNewWeather = e => {
     e.preventDefault()
     this.getWeatherData()
   }
 
-
   render() {
     return (
       <SearchContainer>
-        {this.state.displayWeatherData ?
-          <CurrentWeather
-            currentWeather={this.state.currentWeather} />
-          :
-          <form className="form-container" onSubmit={(e) => this.handleNewWeather(e)}>
+        {this.state.displayWeatherData ? (
+          <CurrentWeather currentWeather={this.state.currentWeather} />
+        ) : (
+          <form
+            className="form-container"
+            onSubmit={e => this.handleNewWeather(e)}>
             <div class="form-group">
               <label for="exampleInputEmail1">Zip Code</label>
               <input
-                required pattern="[0-9]{5}"
+                required
+                pattern="[0-9]{5}"
                 type="text"
                 class="form-control form-control-lg"
                 id="exampleInputEmail1"
@@ -56,15 +62,19 @@ export default class WeatherSearch extends Component {
                 name="currentZip"
                 placeholder="Please enter a 5-digit zip code (U.S. Only)"
                 onChange={this.handleChange}
-                value={this.state.currentZip.currentZip}>
-              </input>
-              <small id="emailHelp" class="form-text text-muted">We do not store any of your location information.</small>
+                value={this.state.currentZip.currentZip}
+              />
+              <small id="emailHelp" class="form-text text-muted">
+                We do not store any of your location information.
+              </small>
             </div>
             <div className="button-div">
-              <button type="submit" class="btn btn-primary">Get Weather</button>
+              <button type="submit" class="btn btn-primary">
+                Get Weather
+              </button>
             </div>
           </form>
-        }
+        )}
       </SearchContainer>
     )
   }
